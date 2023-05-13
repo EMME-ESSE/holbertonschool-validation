47be69995fe4c9c7a5bcc6deb239458779854cae
EMME-ESSE <spiritexdi@gmail.com>
2023-05-13 19:14:46 -0300

    .

--- module2_task2/main_test.go
+++ module2_task2/main_test.go
@@ -7,76 +7,61 @@ import (
   "testing"
 )
 
-func TestIntegration(t *testing.T) {
-    if testing.Short() {
-        t.Skip("Flag `-short` absent: skipping Integration Tests.")
-    }
+func Test_HelloHandler(t *testing.T) {
+  if !testing.Short() {
+    t.Skip("Flag `-short` absent: skipping Unit Tests.")
+  }
 
-    // create new http server with router
-    server := httptest.NewServer(setupRouter())
-    defer server.Close()
+  tests := []struct {
+    name         string
+    queryString  string
+    responseCode int
+    body         string
+  }{
+    {
+      name:         "Grace Hopper",
+      queryString:  "name=Grace Hopper",
+      responseCode: 200,
+      body:         "Hello Grace Hopper!",
+    },
+    {
+      name:         "Rosalind Franklin",
+      queryString:  "name=Rosalind Franklin",
+      responseCode: 200,
+      body:         "Hello Rosalind Franklin!",
+    },
+    // INSERT MORE TESTS HERE
+  }
+  for _, tt := range tests {
+    t.Run(tt.name, func(t *testing.T) {
 
-    tests := []struct {
-        name         string
-        queryString  string
-        responseCode int
-        body         string
-    }{
-        {
-            name:         "Grace Hopper",
-            queryString:  "name=Grace Hopper",
-            responseCode: 200,
-            body:         "Hello Grace Hopper!",
-        },
-        {
-            name:         "Rosalind Franklin",
-            queryString:  "name=Rosalind Franklin",
-            responseCode: 200,
-            body:         "Hello Rosalind Franklin!",
-        },
-        {
-            name:         "No name provided",
-            queryString:  "",
-            responseCode: 400,
-            body:         "Bad Request: Missing 'name' parameter",
-        },
-        // Add more test cases here
-    }
+      // Build an HTTP request for the current test `tt`
+      req, err := http.NewRequest("GET", "/hello", nil)
+      if err != nil {
+        t.Fatal(err)
+      }
 
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            // Build an HTTP request for the current test `tt`
-            req, err := http.NewRequest("GET", "/hello", nil)
-            if err != nil {
-                t.Fatal(err)
-            }
+      queryParams, _ := url.ParseQuery(tt.queryString)
+      req.URL.RawQuery = queryParams.Encode()
 
-            queryParams, _ := url.ParseQuery(tt.queryString)
-            req.URL.RawQuery = queryParams.Encode()
+      // Initialize a new httptest serve with the function under test
+      rr := httptest.NewRecorder()
+      handler := http.HandlerFunc(HelloHandler)
+      handler.ServeHTTP(rr, req)
 
-            // Send HTTP request to the test server
-            res, err := http.DefaultClient.Do(req)
-            if err != nil {
-                t.Fatal(err)
-            }
+      // Check that the status code is what you expect.
+      expectedCode := tt.responseCode
+      gotCode := rr.Code
+      if gotCode != expectedCode {
+        t.Errorf("handler returned wrong status code: got %v want %v", gotCode, expectedCode)
+      }
 
-            // Check that the status code is what you expect.
-            expectedCode := tt.responseCode
-            if res.StatusCode != expectedCode {
-                t.Errorf("handler returned wrong status code: got %v want %v", res.StatusCode, expectedCode)
-            }
-
-            // Check that the response body is what you expect.
-            expectedBody := tt.body
-            buf := new(bytes.Buffer)
-            _, err = buf.ReadFrom(res.Body)
-            if err != nil {
-                t.Fatal(err)
-            }
-            gotBody := buf.String()
-            if gotBody != expectedBody {
-                t.Errorf("handler returned unexpected body: got %v want %v", gotBody, expectedBody)
-            }
-        })
-    }
+      // Check that the response body is what you expect.
+      expectedBody := tt.body
+      gotBody := rr.Body.String()
+      if gotBody != expectedBody {
+        t.Errorf("handler returned unexpected body: got %v want %v", gotBody, expectedBody)
+      }
+    })
+  }
 }
